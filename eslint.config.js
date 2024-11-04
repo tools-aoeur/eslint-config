be6584033a3115a731f285fa4d03f866c0bbe10f
Jan Amann <jan@amann.work>
2024-11-04 18:04:06 +0100

    remove globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,16 @@
 // @ts-check
 
-import globals from 'globals';
 import { getPresets } from './src/index.js';
 
 /**
  * @type {Array<import('eslint').Linter.Config>}
  */
-export default (
-  await getPresets(
-    'javascript',
-    'react',
-    'cssModules',
-    'jest',
-    'cypress',
-    'vitest',
-    'lodash'
-  )
-).concat({
-  languageOptions: {
-    globals: globals.node
-  }
-});
+export default await getPresets(
+  'javascript',
+  'react',
+  'cssModules',
+  'jest',
+  'cypress',
+  'vitest',
+  'lodash'
+);
