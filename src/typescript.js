265f64862c0432e8dcbdee5a67538c36a35ea991
Andreas Schacker <menelaos@users.noreply.github.com>
2024-11-05 13:26:29 +0100

    Enable `no-import-type-side-effects` rule

--- src/typescript.js
+++ src/typescript.js
@@ -57,6 +57,7 @@ export default [
       '@typescript-eslint/await-thenable': ERROR,
       '@typescript-eslint/method-signature-style': [ERROR, 'property'],
       '@typescript-eslint/no-for-in-array': ERROR,
+      '@typescript-eslint/no-import-type-side-effects': ERROR,
       '@typescript-eslint/no-misused-promises': [
         ERROR,
         { checksVoidReturn: { attributes: false } }
